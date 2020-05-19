8c95fe8ecd620317654b6f7a9992ac10502ed39b
Saptarshi Guha <saptarshi.guha@gmail.com>
2020-05-19 16:17:01 -0700

    suggestions accepted

--- run.sh
+++ run.sh
@@ -15,6 +15,6 @@ else
     ## Quick Check if RUNFAST environment is passed
     Rscript -e "setwd('/webrender_intel_win10_nightly'); source('query.R');g=bq(use_email='no'); g$q('select client_id from `moz-fx-data-shared-prod`.telemetry.main where date(submission_timestamp)=\'2020-01-01\' limit 10')"
     bq query --use_legacy_sql=false "select client_id from `moz-fx-data-shared-prod`.telemetry.main where date(submission_timestamp)='2020-01-01'"
-    gsutil cp /webrender_intel_win10_nightly/driver.Rmd gs://moz-fx-data-prod-analysis/sguha/ds_283/
+    gsutil cp /webrender_intel_win10_nightly/driver.Rmd $BUCKET/sguha/ds_283/
 fi
 
