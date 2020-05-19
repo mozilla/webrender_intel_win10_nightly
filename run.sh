0dd81286ad88c68506ed0f4c26023c0e0fd69e58
Saptarshi Guha <saptarshi.guha@gmail.com>
2020-05-19 15:28:33 -0700

    credential handling fixed

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 BUCKET=moz-fx-data-prod-analysis
 gcloud auth activate-service-account --key-file /app/.credentials
 
-Rscript -e "setwd('/webrender_intel_win10_nightly');library(rmarkdown); render('driver.Rmd',param=list(args=list(use_email=FALSE)));render('dashboard.Rmd',output_dir='/tmp/output/')"
+Rscript -e "setwd('/webrender_intel_win10_nightly');library(rmarkdown); render('driver.Rmd',param=list(args=list(use_email='use')));render('dashboard.Rmd',output_dir='/tmp/output/')"
 gsutil -m  rsync -r -d /tmp/output/   gs://$BUCKET/sguha/ds_283/
 gsutil -m  rsync -r  /webrender_intel_win10_nightly/driver.html   gs://$BUCKET/sguha/ds_283/
