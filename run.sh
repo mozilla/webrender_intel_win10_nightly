e093a955b5c66938a2cb177a33f9fdedbc0352da
Saptarshi Guha <saptarshi.guha@gmail.com>
2020-05-19 20:40:39 -0700

    Confirmed that changes work _a_ service account

--- run.sh
+++ run.sh
@@ -14,14 +14,14 @@ fi
 if [[ -z "${RUNFAST}" ]]; then   
     ## Complete run if RUNFAST environment is missing
     echo "Running SLOW"
-    Rscript /webrender_intel_win10_nightly/runner.R
     gsutil -m  rsync -r -d /tmp/output/   $BUCKET/sguha/ds_283/
     gsutil -m  rsync -r  /webrender_intel_win10_nightly/driver.html   $BUCKET/sguha/ds_283/
+    Rscript /webrender_intel_win10_nightly/runner.R
 else
     ## Quick Check if RUNFAST environment is passed
     echo "Running FAST"
-    #Rscript /webrender_intel_win10_nightly/test.R 
     bq query --project_id="$PROJECT_ID" --use_legacy_sql=false "select client_id from \`moz-fx-data-shared-prod\`.telemetry.main where date(submission_timestamp)='2020-01-01' limit 10"
     gsutil cp /webrender_intel_win10_nightly/driver.Rmd $BUCKET/sguha/ds_283/
+    Rscript /webrender_intel_win10_nightly/test.R 
 fi
 
